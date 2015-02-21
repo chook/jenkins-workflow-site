d20a4a988f866e0f21aed3df8bda17952acca285
Chen Harel <chen.harel@takipi.com>
2015-02-21 12:57:56 +0200

    fixes

--- flow.groovy
+++ flow.groovy
@@ -13,13 +13,13 @@ node {
   stage 'Package'
 
   sh "./activator dist"
-  archive 'target/universal/*.zip'
-  //step([$class: 'ArtifactArchiver', artifacts: '**/target/universal/*.zip', fingerprint: true])
+  archive "target/universal/*.zip"
+
   echo "chen1"
   newname = buildname("target/universal/siteplay-1.0-SNAPSHOT.zip")
   echo "chen2: " + newname
 }
 
 def buildname(name) {
-  name - ".zip" + "-" + "${env.BUILD_NUMBER}" + ".zip"
+  name - ".zip" + "-${env.BUILD_NUMBER}.zip"
 }
