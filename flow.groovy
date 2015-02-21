068ee700e4a8857ed76365a4ab83e56da6daffaf
Chen Harel <chen.harel@takipi.com>
2015-02-21 12:53:54 +0200

    bring back the node

--- flow.groovy
+++ flow.groovy
@@ -1,23 +1,24 @@
 echo 'hello from Workflow'
+node {
+  stage 'Compile+Test'
+  git url: '/Users/chenharel/Sparktale/Git/takipi-dev/site', branch: 'develop'
+  try {
+    sh "./activator compile test"
+  } catch (_) {
+    echo "EXCEPTION"
+  }
 
-stage 'Compile+Test'
-git url: '/Users/chenharel/Sparktale/Git/takipi-dev/site', branch: 'develop'
-try {
-  sh "./activator compile test"
-} catch (_) {
-  echo "EXCEPTION"
+  step([$class: 'JUnitResultArchiver', testResults: '**/target/test-reports/*.xml'])
+
+  stage 'Package'
+
+  sh "./activator dist"
+  archive 'target/universal/*.zip'
+  //step([$class: 'ArtifactArchiver', artifacts: '**/target/universal/*.zip', fingerprint: true])
+  echo "chen1"
+  newname = buildname("target/universal/siteplay-1.0-SNAPSHOT.zip")
+  echo "chen2: " + newname
 }
-
-step([$class: 'JUnitResultArchiver', testResults: '**/target/test-reports/*.xml'])
-
-stage 'Package'
-
-sh "./activator dist"
-archive 'target/universal/*.zip'
-//step([$class: 'ArtifactArchiver', artifacts: '**/target/universal/*.zip', fingerprint: true])
-echo "chen1"
-newname = buildname("target/universal/siteplay-1.0-SNAPSHOT.zip")
-echo "chen2: " + newname
 
 def buildname(name) {
   name - ".zip" + "-" + "${env.BUILD_NUMBER}" + ".zip"
